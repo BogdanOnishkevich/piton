f618f9878b2dd5793663eae86dcbef393c8515ae
BogdanOnishkevich <37015375+BogdanOnishkevich@users.noreply.github.com>
2018-03-03 17:21:48 +0600

    создал две программы определитель  дня .

---
+++ program.py
@@ -0,0 +1,10 @@
+age = int (input ("сколько лет?"))
+age = 2018 - age
+print (age)
+
+
+
+
+
+    
+    
---
+++ program2.py
@@ -0,0 +1,17 @@
+time = int (input ("ввидите время"))
+if time >= 6:00 and time <= 9:00:
+    print (" утро")
+elif time >= 9:00 and time <= 12:00:
+    print ("день")
+elif time >= 12:00 and time <=15:00:
+    print ("обед")
+elif time >= 15:00 and time <=18:00:
+    print ("полдень ")
+elif time >= 18:00 and time <= 21:00:
+    print("вечер")
+elif time >=21:00 and time <= 24:00:
+    print ("полночь")
+elif time >=24:00 and time <= 6:00:
+    print ("ночь")
+    
+    
